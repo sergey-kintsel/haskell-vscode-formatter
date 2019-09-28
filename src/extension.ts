28f45c3d320c78a7bd12bc6bc3253aac19d618f8
Eric White <eric@ericwhite.ca>
2019-09-28 13:37:35 +0100

    Support workspace level configuration files.

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,19 @@ const haskellLangId = 'haskell';
 export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerDocumentRangeFormattingEditProvider(haskellLangId, {
     provideDocumentRangeFormattingEdits(document, range, options, token) {
+      const cwd = vscode.workspace.rootPath;
       const showErrorMessage = (friendlyText, error) => {
         vscode.window.showErrorMessage(`${friendlyText}\n${error.stderr.toString()}`);
         return [];
       }
 
       try {
-        proc.execSync('hindent --help');
+        proc.execSync('hindent --help', {cwd: cwd});
       } catch (e) {
         return showErrorMessage("HIndent is not installed", e);
       }
       try {
-        proc.execSync('stylish-haskell --help');
+        proc.execSync('stylish-haskell --help', {cwd: cwd});
       } catch (e) {
         return showErrorMessage("stylish-haskell is not installed", e);
       }
@@ -24,13 +25,13 @@ export function activate(context: vscode.ExtensionContext) {
       const text = document.getText(range);
       let hindentedText;
       try {
-        hindentedText = proc.execSync('hindent', {input: text}).toString();
+        hindentedText = proc.execSync('hindent', {input: text, cwd: cwd}).toString();
       } catch (e) {
         return showErrorMessage('hindent failed to format the code', e);
       }
       let styledText;
       try {
-        styledText = proc.execSync('stylish-haskell', {input: hindentedText}).toString();
+        styledText = proc.execSync('stylish-haskell', {input: hindentedText, cwd: cwd}).toString();
       } catch (e) {
         return showErrorMessage('stylish-haskell failed to format the code', e);
       }
